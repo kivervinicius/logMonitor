3cdbeeb3b119fd03bada0f19bd409006af9268f6
Kiver <kiver@eu.com>
2016-10-03 17:22:23 -0400

    Adicionada notificação quando encontrado um problema nos logs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-var socket = io('http://kiver-pc:8191');
+//var socket = io('http://kiver-pc:8191');
+var socket = io('https://monitor.omegasistemas.net.br', {transports: ['websocket', 'polling', 'flashsocket']});
 
 socket.on('init', function(obj){
     $(".path").text(obj.path); 
@@ -13,12 +14,41 @@ socket.on('loadFile', function(obj){
     $("#" +extractFileName(obj.file)).html(obj.data);
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.'); 
+        return;
+    }
+
+    if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+    }
+    
+});
+
+function notify(msg, titulo, onclick) {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        var notification = new Notification("Erro - " + titulo , {
+            body: msg
+        });
+
+        notification.onclick = function(){
+            window.focus();
+            if (onclick) {
+                onclick.call(window);
+            }
+        };
+    }
+}
+
 
 function buildMenus(files){
     var menu = "", 
         content = "";
     files.forEach(function(file){
-        menu += `<li>
+        menu += `<li id="mn-${extractFileName(file)}">
                     <a href="#${extractFileName(file)}">
                         ${file}
                     </a>
@@ -31,10 +61,27 @@ function buildMenus(files){
     actions();  
 };
 
+var error = false;
 function onChange(obj){
-    var pre = $("#" +extractFileName(obj.file));
+    var filename = extractFileName(obj.file),
+        pre = $("#" + filename),
+        el = $("#mn-"+ filename + " a");
     pre.html(pre.html() + "\n" + obj.data);
     
+    if (obj.data.indexOf("ERROR ") > 0) {
+        if (!error) {
+            notify(obj.data,  filename, function() {
+                el.click();
+            });
+            error = true;
+            setTimeout(() => {
+                error = false;
+            }, 1000 * 60);
+        } else {
+            console.log(filename, obj.data);
+        }
+        $("#mn-"+ filename + " a").addClass("error");
+    } 
 }
 
 function actions() {
